eb54a9a7c739c406c55cb67a6336a73ac260ff86
KilimnikRuslan <kulumnuk_2000@ukr.net>
2023-09-09 13:51:03 +0300

    перероблена дз

--- lesson17.js
+++ lesson17.js
@@ -1,34 +1,27 @@
-function filter(arr, from, to) {
-    return arr.filter(function(number) {
-        return number >= from && number <= to;
-    });
-}
+const filter = (arr, from, to) => {
+  return arr.filter(number => number >= from && number <= to);
+};
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const filtered = filter(numbers, 3, 7);
 
-alert(filtered); 
-
+alert(filtered);
 // -------------------------------------------------------------------------------------------------------------------
 
-function sortArrayAscending(arr) {
-    return arr.sort(function(a, b) {
-        return a - b;
-    });
-}
+const sortArrayAscending = (arr) => {
+  return arr.sort((a, b) => a - b);
+};
 
-const numbers = [56, 18, 28, 42, 14, 4, 11, 15, 3, 10, 8, 13, 6];
+const numbers = [56, 18, 28, 42, 14, 4, 11, 15, -24, 3, 10, 8, 13, 6];
 const sortedNumbers = sortArrayAscending(numbers);
 
 alert(sortedNumbers);
 
 // -----------------------------------------------------------------------------------------------------------------
 
-function sortByLength(arr) {
-    return arr.sort(function(a, b) {
-        return a.length - b.length;
-    });
-}
+const sortByLength = (arr) => {
+  return arr.sort((a, b) => a.length - b.length);
+};
 
 let strings = ["22", "23456", "2222", "333", "2", "234567777", "3456723456"];
 let sortedStrings = sortByLength(strings);
@@ -62,33 +55,26 @@ const arr = [
 
 
 const arr = [
-    { name: "John", age: 25 },
-    { name: "Pete", age: 30 },
-    { name: "Mary", age: 29 },
-    { name: "Taras", age: 25 },
-    { name: "Kate", age: 74 },
-    { name: "Chris", age: 14 },
-    { name: "Alan", age: 5 },
-    { name: "Boris", age: 55 },
-    { name: "Elizabeth", age: 48 },
-  ];
-  
-  function findMinMax(arr) {
-    if (arr.length === 0) {
-      return { min: undefined, max: undefined };
-    }
-  
-    return arr.reduce((acc, curr) => {
-      if (acc.min === undefined || curr.age < acc.min) {
-        acc.min = curr.age;
-      }
-      if (acc.max === undefined || curr.age > acc.max) {
-        acc.max = curr.age;
-      }
-      return acc;
-    }, { min: undefined, max: undefined });
-  }
-  
-  const result = findMinMax(arr);
-  
-  alert(`Мінімальний вік: ${result.min},Максимальний вік: ${result.max}`);
+  { name: "John", age: 25 },
+  { name: "Pete", age: 30 },
+  { name: "Mary", age: 29 },
+  { name: "Taras", age: 25 },
+  { name: "Kate", age: 74 },
+  { name: "Chris", age: 14 },
+  { name: "Alan", age: 5 },
+  { name: "Boris", age: 55 },
+  { name: "Elizabeth", age: 48 },
+];
+
+const findMinMax = (arr) =>
+  arr.reduce(
+    (acc, curr) => ({
+      min: acc.min === undefined || curr.age < acc.min ? curr.age : acc.min,
+      max: acc.max === undefined || curr.age > acc.max ? curr.age : acc.max,
+    }),
+    { min: undefined, max: undefined }
+  );
+
+const result = findMinMax(arr);
+
+alert(`Мінімальний вік: ${result.min}, Максимальний вік: ${result.max}`);
