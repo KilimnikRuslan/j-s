bab8a6063df52c2555225eee3bafc8eb11d5d0c6
KilimnikRuslan <kulumnuk_2000@ukr.net>
2023-10-19 22:20:21 +0300

    Pointer

---
+++ lesson17.js
@@ -0,0 +1,14 @@
+const handleMouseClick = (event) => {
+    const point = document.createElement("div");
+    point.className = "point";
+  
+    let leftPosition = event.clientX - 5;
+    let topPosition = event.clientY - 5;
+  
+    point.style.left = `${leftPosition}px`;
+    point.style.top = `${topPosition}px`;
+  
+    document.body.appendChild(point);
+  };
+  
+  document.addEventListener("click", handleMouseClick);
